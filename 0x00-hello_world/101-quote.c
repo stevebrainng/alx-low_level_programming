66635c3b0cfec9c57f58911879dbea17c72d0d36
Godwin Stephen <info.sosoloaded@gmail.com>
2022-10-14 04:50:15 -0700

    101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,6 @@
  */
 int main(void)
 {
-	int c;
-
-	c = write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
