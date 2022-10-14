5413077baa006b3a212afcf9db2b56438b4c8dc1
Godwin Stephen <info.sosoloaded@gmail.com>
2022-10-13 21:29:33 -0700

    Write a C program that prints exactly the given text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * printf() - use the printf function to display text
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
