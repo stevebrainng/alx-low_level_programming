fbdf6365d6a4a565d9c0cafe199e4d11ac29ca11
Godwin Stephen <info.sosoloaded@gmail.com>
2022-10-13 21:18:19 -0700

    C program that uses puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entery coding point
+ * char myline - is a variable that hold my text
+ * Return: Always 0 (Successs)
+ */
+int main(void)
+{
+	char myline [] = "Programming is like building a multilingual puzzle\";
+	puts(myline);
+		return (0);
+}
