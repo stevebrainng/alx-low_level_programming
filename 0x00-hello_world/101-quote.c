e50c61a82e37a3adb24223c19fd40218932b1f65
Godwin Stephen <info.sosoloaded@gmail.com>
2022-10-14 04:48:15 -0700

    101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always (1) Success
+ */
+int main(void)
+{
+	int c;
+
+	c = write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
