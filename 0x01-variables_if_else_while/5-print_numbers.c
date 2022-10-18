fba22e1ba2ce3af08af83bfe914d5d849fa85300
Godwin Stephen <info.sosoloaded@gmail.com>
2022-10-17 20:20:48 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Program Entery point
+ *
+ * Return: 0 on success or of different number on error
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; a <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
