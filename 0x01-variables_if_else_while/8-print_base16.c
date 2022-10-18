5958af1040b9db8cdc794b4ef2d6628e0a4bd6e7
Godwin Stephen <info.sosoloaded@gmail.com>
2022-10-17 20:42:34 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: 0 if okay, non zero if error
+ */
+int main(void)
+{
+	int num;
+	char bb;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	
+	for (bb = 'a'; bb <= 'f'; bb++)
+		putchar(bb);
+
+	putchar('\n');
+	return (0);
+}
