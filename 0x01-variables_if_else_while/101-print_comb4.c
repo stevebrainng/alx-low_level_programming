3f8afa91aa283b9fe4f8fd5890bcd01c03ff0b70
Godwin Stephen <info.sosoloaded@gmail.com>
2022-10-17 21:10:58 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: 0 on success, non zero fail
+ */
+int main(void)
+{
+	int m, i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; k <= '9'; j++)
+		{
+			for (m = '0'; m <= '9'; m++)
+			{
+				if (i < j && j < m)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(m);
+					if (i == '7' && m == '9')
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
