f8e0d9be05716462d6a8ba151959d4a74c29b597
Godwin Stephen <info.sosoloaded@gmail.com>
2022-10-17 20:30:56 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: 0 no error, non zero if error
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0';i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
