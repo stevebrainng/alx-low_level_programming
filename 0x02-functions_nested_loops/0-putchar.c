a6c992ac17cfcfe363484805febbeb9790a4d044
Godwin Stephen <info.sosoloaded@gmail.com>
2022-10-19 19:56:23 -0700

    0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchat('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
